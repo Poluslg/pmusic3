8f3695744bdfe6613db37988abaff45308f7df1a
Poluslg <prahladbiswas08@gmail.com>
2023-03-22 18:04:47 +0530

    addsome jsx and css

--- src/Components/Afterlogin.jsx
+++ src/Components/Afterlogin.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
-import Header from "./Header";
-import "./Afterlogin.css"
-import Login from './Login';
-function Afterlogin() {
- 
+import React from 'react'
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+
+export default function Afterlogin() {
   return (
-    <div>
-      <Header/>
-      got it
+    <div className='Container'>
+      <div className="spotify__body">
+        <Sidebar />
+        <div className="body">
+          <Header />
+          <div className="body__contents">
+            <Body />
+          </div>
+        </div>
+      </div>
+      <div className="spotify__footer">
+        <Footer />
+      </div>
     </div>
   )
 }
-
-export default Afterlogin
-
